642b01cc93b2a130855829b7331711c4345c9296
Marco Mazzai <mazzai.marco@gmail.com>
2020-12-26 14:47:22 +0100

    fix project

--- index.js
+++ index.js
@@ -1,92 +1,92 @@
 ///VARIABLES///
-var myCalculator = new Calculator('display'),
-    operatorControls = document.getElementsByClassName('operator'),
-    numberControls = document.getElementsByClassName('number');
+const myCalculator = new Calculator('display')
+const operatorControls = document.getElementsByClassName('operator')
+const numberControls = document.getElementsByClassName('number')
 
 ///ASSIGNMENTS///
 document.getElementById('ac').addEventListener('click', function() {
-  myCalculator.allClear();
-});
+  myCalculator.allClear()
+})
 
 document.getElementById('plusMinus').addEventListener('click', function() {
-  myCalculator.plusMinus();
-});
+  myCalculator.plusMinus()
+})
 
 document.getElementById('=').addEventListener('click', function() {
-  myCalculator.getTotal();
-});
+  myCalculator.getTotal()
+})
 
 for (var i = 0; i < operatorControls.length; i++) {
   operatorControls[i].addEventListener('click', function() {
-    myCalculator.handleOperator(this.getAttribute('id'));
-  });
+    myCalculator.handleOperator(this.getAttribute('id'))
+  })
 }
 
 for (i = 0; i < numberControls.length; i++) {
   numberControls[i].addEventListener('click', function() {
-    myCalculator.handleNumber(this.getAttribute('id'));
-  });
+    myCalculator.handleNumber(this.getAttribute('id'))
+  })
 }
 
 ///OBJECTS///
 function Calculator(displayId){
-  this.displayId = displayId;
-  this.operationArray = [];
+  this.displayId = displayId
+  this.operationArray = []
 };
 
 Calculator.prototype.updateDisplay = function(){
-  document.getElementById(this.displayId).innerText = this.operationArray.join(' ');
-};
+  document.getElementById(this.displayId).innerText = this.operationArray.join(' ')
+}
 
 Calculator.prototype.handleNumber = function(number){
   if (isNaN(this.getLastItem())){
-    this.operationArray.push(number.toString());
+    this.operationArray.push(number.toString())
   } else {
-    this.addToLast(number.toString());
+    this.addToLast(number.toString())
   }
-  this.updateDisplay();
+  this.updateDisplay()
 };
 
 Calculator.prototype.addToLast = function(input){
-  this.operationArray[this.operationArray.length - 1] += input;
+  this.operationArray[this.operationArray.length - 1] += input
 };
 
 Calculator.prototype.getLastItem = function(){
-  return this.operationArray[this.operationArray.length - 1];
-};
+  return this.operationArray[this.operationArray.length - 1]
+}
 
 Calculator.prototype.handleOperator = function(operator){
   if (!isNaN(this.getLastItem())){
     if (operator === ','){
-        this.addToLast(operator);
+        this.addToLast(operator)
       } else {
-        this.operationArray.push(operator);
+        this.operationArray.push(operator)
       }
     }
-  this.updateDisplay();
-};
+  this.updateDisplay()
+}
 
 Calculator.prototype.plusMinus = function(){
   if(!isNaN(this.getLastItem())){
     if (this.operationArray[0] === '-'){
       this.operationArray.shift();
     } else {
-      this.operationArray.unshift('-');
+      this.operationArray.unshift('-')
     }
   }
-  this.updateDisplay();
+  this.updateDisplay()
 };
 
 Calculator.prototype.allClear = function(funcName){
-  this.operationArray = [];
-  this.updateDisplay();
+  this.operationArray = []
+  this.updateDisplay()
 };
 
 Calculator.prototype.getTotal = function(){
   if (isNaN(this.getLastItem())){
-    this.operationArray.pop();
+    this.operationArray.pop()
   }
   var total = eval(this.operationArray.join(''));
-  this.operationArray = [total];
-  this.updateDisplay();
+  this.operationArray = [total]
+  this.updateDisplay()
 };
